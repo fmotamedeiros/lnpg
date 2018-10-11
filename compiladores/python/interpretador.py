c5287761fe68cb5a386d5a47c7ddd062698014d6
Daniel Yohan <daniel@fontedeprecos.com.br>
2018-10-11 13:34:44 -0300

    Refatoração do codigo

--- compiladores/python/interpretador.py
+++ compiladores/python/interpretador.py
@@ -1,11 +1,20 @@
-from parser import *
-from lexer import *
+from parser import Parser
+from lexer import Lexer
 
-code = 'A = B + C; C = B; A = B - C;'
-tokens = analise_lexica(code, tokens_da_linguagem)
-print(tokens)
-programa = analise_sintatica(tokens)
-print (programa)
+class Interpretador:
+    def __init__(self):
+        self.lexer = Lexer()
+        self.parser = Parser()
+        self.run()
 
-for token in programa:
-    print(token.interpretar())
+    def run(self):
+        code = 'A = B + C; C = B; A = B - C;'
+        tokens = self.lexer.analise_lexica(code, Lexer().tokens_da_linguagem)
+        print(tokens)
+        programa = self.parser.analise_sintatica(tokens)
+        print (programa)
+        for token in programa:
+            print(token.interpretar())
+
+if __name__ == "__main__":
+    Interpretador()
--- compiladores/python/parser.py
+++ compiladores/python/parser.py
@@ -1,5 +1,3 @@
-import sys
-
 class Token:
     def interpretar(self):
         return None
@@ -14,37 +12,41 @@ class Atribuicao (Token):
     def interpretar(self):
         if self.c:
             return self.a + ' = ' + self.b + self.operacao + self.c + ';'
-        return self.a + ' = ' + self.b + ';' 
+        return self.a + ' = ' + self.b + ';'
 
-def atribuicao (tokens, pos):
-    if len(tokens) >= (pos+4):
-        if tokens[pos][1] == 'VAR' and tokens[pos+1][1] == 'IGUAL' and tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SEPARADOR':
-            return ((pos+4), Atribuicao(tokens[pos][0], tokens[pos+2][0], None, None))
-    if len(tokens) >= (pos+6):
-        if tokens[pos][1] == 'VAR' and tokens[pos+1][1] == 'IGUAL':
-            if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SOMA' and tokens[pos+4][1] == 'VAR':
-                if tokens[pos+5][1] == 'SEPARADOR':
-                    return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '+'))
-                if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SUB' and tokens[pos+4][1] == 'VAR':
-                    if tokens[pos+5][1] == 'SEPARADOR':
-                        return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '-'))
-                if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'MULTIPLICACAO' and tokens[pos+4][1] == 'VAR':
-                    if tokens[pos+5][1] == 'SEPARADOR':
-                        return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '*'))
-                if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'DIVISAO' and tokens[pos+4][1] == 'VAR':
-                    if tokens[pos+5][1] == 'SEPARADOR':
-                        return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '/'))
-    return (pos, None)
+class Parser:
+    def __init__(self):
+        pass
 
-def analise_sintatica(tokens):
-    pos = 0
-    programa = []
-    while pos < len(tokens):
-        posAtual, att = atribuicao(tokens, pos)
-        if pos != posAtual:
-            programa.append(att)
-            pos = posAtual
-        else:
-            sys.stderr.write('Problema no programa: %s\n' % pos)
-            sys.exit(1)
-    return programa
+    def atribuicao (self, tokens, pos):
+        if len(tokens) >= (pos+4):
+            if tokens[pos][1] == 'VAR' and tokens[pos+1][1] == 'IGUAL' and tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SEPARADOR':
+                return ((pos+4), Atribuicao(tokens[pos][0], tokens[pos+2][0], None, None))
+        if len(tokens) >= (pos+6):
+            if tokens[pos][1] == 'VAR' and tokens[pos+1][1] == 'IGUAL':
+                if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SOMA' and tokens[pos+4][1] == 'VAR':
+                    if tokens[pos+5][1] == 'SEPARADOR':
+                        return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '+'))
+                    if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'SUB' and tokens[pos+4][1] == 'VAR':
+                        if tokens[pos+5][1] == 'SEPARADOR':
+                            return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '-'))
+                    if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'MULTIPLICACAO' and tokens[pos+4][1] == 'VAR':
+                        if tokens[pos+5][1] == 'SEPARADOR':
+                            return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '*'))
+                    if tokens[pos+2][1] == 'VAR' and tokens[pos+3][1] == 'DIVISAO' and tokens[pos+4][1] == 'VAR':
+                        if tokens[pos+5][1] == 'SEPARADOR':
+                            return ((pos+6), Atribuicao(tokens[pos][0], tokens[pos+2][0], tokens[pos+4][0], '/'))
+        return (pos, None)
+
+    def analise_sintatica(self, tokens):
+        pos = 0
+        programa = []
+        while pos < len(tokens):
+            posAtual, att = self.atribuicao(tokens, pos)
+            if pos != posAtual:
+                programa.append(att)
+                pos = posAtual
+            else:
+                __import__("sys").stderr.write('Problema no programa: %s\n' % pos)
+                __import__("sys").exit(1)
+        return programa
